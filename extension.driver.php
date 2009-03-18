2f262926a9862c16e7a2830655495856ee1ee03a
Carsten de Vries <carsten@fleshy.nl>
2009-03-18 13:45:20 +0100

    First release

---
+++ extension.driver.php
@@ -0,0 +1,193 @@
+<?php
+
+	require_once(TOOLKIT . '/class.datasource.php');
+	
+	class Extension_GlobalParamLoader extends Extension {
+	/*-------------------------------------------------------------------------
+		Definition:
+	-------------------------------------------------------------------------*/
+		
+		public static $params = array();
+		
+		public function about() {
+			return array(
+				'name'			=> 'Global Parameter Loader',
+				'version'		=> '1.0',
+				'release-date'	=> '2009-03-18',
+				'author'		=> array(
+					'name'			=> 'Carsten de Vries',
+					'website'		=> 'http://www.vrieswerk.nl',
+					'email'			=> 'carsten@vrieswerk.nl'
+				),
+				'description'	=> 'Allows you to add parameters to Symphony\'s parameter pool.'
+	 		);
+		}
+		
+		public function uninstall() {
+			$this->_Parent->Configuration->remove('globalparamloader');
+			$this->_Parent->Database->query("DROP TABLE `tbl_gpl_sets`");
+			$this->_Parent->Database->query("DROP TABLE `tbl_gpl_params`");
+			$this->_Parent->saveConfig();
+		}
+		
+		public function install() {
+			$this->_Parent->Database->query("
+				CREATE TABLE IF NOT EXISTS `tbl_gpl_sets` (
+					`id` int(11) unsigned NOT NULL auto_increment,
+					`name` varchar(255) NOT NULL,
+					`params` int(11) unsigned,
+					`exclude_page` varchar(255),
+					PRIMARY KEY (`id`)
+				)
+			");
+			
+			$this->_Parent->Database->query("
+				CREATE TABLE IF NOT EXISTS `tbl_gpl_params` (
+					`id` int(11) NOT NULL auto_increment,
+					`set_id` int(11) NOT NULL,
+					`param` varchar(255) NOT NULL,
+					`value` varchar(255),
+					`type` varchar(255) NOT NULL,
+					`sortorder` int(11) NOT NULL,
+					PRIMARY KEY (`id`)
+				)
+			");
+			
+			return true;
+		}
+		
+		public function getSubscribedDelegates() {
+			return array(
+				array(
+					'page'		=> '/frontend/',
+					'delegate'	=> 'FrontendParamsResolve',
+					'callback'	=> 'addParam'
+				),
+				array(
+					'page'		=> '/system/sets/',
+					'delegate'	=> 'AddCustomPreferenceFieldsets',
+					'callback'	=> 'sets'
+				)
+			);
+		}
+		
+		public function fetchNavigation() {
+			return array(
+				array(
+					'location'	=> 200,
+					'name'	=> 'Global Parameters',
+					'link'	=> '/sets/'
+				)
+			);
+		}
+		
+		public function addParam(&$context) {
+			$sets = $this->getSets();
+			
+			foreach ($sets as $set) {
+				if(!$this->isPageSelected($context['page_data']['id'], $set['id'])) {
+					$parameters = $this->getParameters($set_id);
+					foreach ($parameters as $parameter) {
+						$context['params'][$parameter['param']] = $parameter['value'];
+					}
+				}
+			}
+		}
+			
+	/*-------------------------------------------------------------------------
+		Utility functions:
+	-------------------------------------------------------------------------*/
+		
+		public function getParameters($set_id = null) {
+			if (is_numeric($set_id)) {
+				return $this->_Parent->Database->fetch("
+					SELECT
+						c.*
+					FROM
+						`tbl_gpl_params` AS c
+					WHERE
+						c.set_id = {$set_id}
+					ORDER BY
+						c.sortorder ASC
+				");
+				
+			} else {
+				return $this->_Parent->Database->fetch("
+					SELECT
+						c.*
+					FROM
+						`tbl_gpl_params` AS c
+					ORDER BY
+						c.sortorder ASC
+				");
+			}
+		}
+		
+		public function getPages() {
+			$pages = $this->_Parent->Database->fetch("
+				SELECT
+					p.*
+				FROM
+					`tbl_pages` AS p
+				ORDER BY
+					`sortorder` ASC
+			");
+			$result = array();
+			
+			foreach ($pages as $page) {
+				$page = (object)$page;
+				$path = '';
+				
+				if ($page->path) {
+					$path = '/' . $page->path;
+				}
+				
+				$path .= '/' . $page->handle;
+				
+				$result[] = (object)array(
+					'id'	=> $page->id,
+					'path'	=> $path
+				);
+			}
+			
+			sort($result);
+			
+			return $result;
+		}
+		
+		public function getSets() {
+			return $this->_Parent->Database->fetch("
+				SELECT
+					s.*
+				FROM
+					`tbl_gpl_sets` AS s
+				ORDER BY
+					s.name ASC
+			");
+		}
+		
+		public function getSet($set_id) {
+			return $this->_Parent->Database->fetchRow(0, "
+				SELECT
+					s.*
+				FROM
+					`tbl_gpl_sets` AS s
+				WHERE
+					s.id = '{$set_id}'
+				LIMIT 1
+			");
+		}
+		
+		public function getParamPages($set_id) {
+			$set = $this->getSet($set_id);
+			
+			return explode(',', $set['exclude_page']);
+		}
+		
+		public function isPageSelected($id, $set_id) {
+			$pages = $this->getParamPages($set_id);
+
+			return in_array($id, $pages);
+		}
+	}
+?>
---
+++ readme.txt
@@ -0,0 +1,38 @@
+Global Parameter Loader
+-------------------------------------------------------------------------------
+
+Version: 1.0
+Author: Carsten de Vries <carsten@vrieswerk.nl>
+Build Date: 18 March 2009
+Requirements: Symphony 2
+
+Allows you to add sets of parameters to Symphony's parameter pool while 
+excluding specified pages.
+
+
+Installation
+-------------------------------------------------------------------------------
+
+1.  Upload the 'globalparamloader' folder in this archive to your Symphony
+  'extensions' folder.
+
+2.  Enable it by selecting the "Global Parameter Loader", choose Enable from 
+  the with-selected menu, then click Apply.
+
+  
+Usage
+-------------------------------------------------------------------------------
+
+1.  View the Blueprints > Global Parameters page to add, edit, and remove 
+  Parameter Sets.
+
+2.  Click the create new button to create your first Parameter Set. Give the 
+  Parameter Set a name.
+
+3.  Click Add item to add a parameter. At least one parameter must be defined 
+  for each set, and each parameter must have a name.
+
+4.  Choose any pages you wish to exclude from this Parameter Set.
+
+5.  Click the Create Set button. Now, go see your newly added parameters at 
+  your pages in Parameter debug mode (add ?debug=params to the page URL).
